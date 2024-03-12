3342bdd71d26bef5b55e12441686afe5f24a718d
Charmaine Lam <c73lam@uwaterloo.ca>
2024-03-12 01:22:12 -0400

    add logic for settings to actually apply

--- ClarifEye/Controllers/AR/ARController.swift
+++ ClarifEye/Controllers/AR/ARController.swift
@@ -47,6 +47,8 @@ class ARController: UIViewController, UIGestureRecognizerDelegate, ARSKViewDeleg
     private var shouldClassify: Bool = true
     private var classificationTimer: Timer?
     
+    private let meterToFootRatio: Float = 3.28084
+    
     deinit {
         removeClassificationTimer()
     }
@@ -303,12 +305,23 @@ extension ARController: ClassificationReceiver {
     
                         // Display the message for the object at the first index; which is the object with the highest hazard score
                         if (i == 0) {
-                            let reportedDepth = smoothedDepth == nil ? "an unknown distance" : String(format: " %.2f m", smoothedDepth!)
+                            var reportedDepth: String
+                            if (smoothedDepth == nil) {
+                                reportedDepth = "an unknown distance"
+                            } else if (self.settings != nil && self.settings!.measurementSystem == .Imperial) {
+                                reportedDepth = String(format: " %.2f m", smoothedDepth! * self.meterToFootRatio)
+                            } else {
+                                reportedDepth = String(format: " %.2f m", smoothedDepth!)
+                            }
+                                
+                            
 //                            let reportedConfidence = String(format: "%.2f % confidence", classification.confidence * 100)
                             let message = "\(classification.label) \(reportedDepth) away"
                                 
                             self.statusViewManager?.showMessage(message, autoHide: true)
-                            self.ttsManager?.speak(message)
+                            if (self.settings != nil && self.settings!.audioOutput) {
+                                self.ttsManager?.speak(message)
+                            }
                         }
                     }
                 }
--- ClarifEye/Controllers/AR/Helpers/TTSManager.swift
+++ ClarifEye/Controllers/AR/Helpers/TTSManager.swift
@@ -4,8 +4,6 @@ class TTSManager: NSObject, ObservableObject, AVSpeechSynthesizerDelegate {
     var synthesizer: AVSpeechSynthesizer = AVSpeechSynthesizer()
     var settings: Settings?
     
-    @Published var utteranceRate: Float = 0.5
-    
     override init() {
         super.init()
         synthesizer.delegate = self
@@ -28,7 +26,7 @@ class TTSManager: NSObject, ObservableObject, AVSpeechSynthesizerDelegate {
             let utterance = AVSpeechUtterance(string: text)
             
             utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.voice.compact.en-US.Samantha")
-            utterance.rate = self.utteranceRate
+            utterance.rate = audioSpeed
             
             self.synthesizer.speak(utterance)
         }
